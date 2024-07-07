cea3257c4605221da2739836da5385505578f682
SindhurMarella <167158939+SindhurMarella@users.noreply.github.com>
2024-07-07 18:03:50 +0530

    Update Newsletter.jsx

--- frontend/src/shared/Newsletter.jsx
+++ frontend/src/shared/Newsletter.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import './newsletter.css'
-import { Container, Row, Col } from 'reactstrap'
-import maleTourist from '../assets/images/male-tourist.png'
+import React from "react";
+import "./newsletter.css";
+import { Container, Row, Col } from "reactstrap";
+import maleTourist from "../assets/images/male-tourist.png";
 
 const NewsLetter = () => {
-   return (
-      <section className='newsletter'>
-         <Container>
-            <Row>
-               <Col lg='6'>
-                  <div className="newsletter__content">
-                     <h2>Subcribe now to get useful traveling information</h2>
+  return (
+    <section className="newsletter">
+      <Container>
+        <Row>
+          <Col lg="6">
+            <div className="newsletter__content">
+              <h2>Receive Exclusive Travel Insights</h2>
 
-                     <div className="newsletter__input">
-                        <input type="email" placeholder='Enter your email' />
-                        <button className="btn newsletter__btn">Subcribe</button>
-                     </div>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Obcaecati adipisici sunt in, provident facere ipsam?
-                     </p>
-                  </div>
-               </Col>
-               <Col lg='6'>
-                  <div className="newsletter__img">
-                     <img src={maleTourist} alt="" />
-                  </div>
-               </Col>
-            </Row>
-         </Container>
-      </section>
-   )
-}
+              <div className="newsletter__input">
+                <input type="email" placeholder="Enter your email" />
+                <button className="btn newsletter__btn">Subcribe</button>
+              </div>
+              <p>
+                Explore exclusive travel insights delivered straight to your
+                inbox. Gain insider tips, destination highlights, and special
+                offers curated to enhance your next adventure.
+              </p>
+            </div>
+          </Col>
+          <Col lg="6">
+            <div className="newsletter__img">
+              <img src={maleTourist} alt="" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
