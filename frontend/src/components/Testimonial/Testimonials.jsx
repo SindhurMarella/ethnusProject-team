ca65032590732a1cfecddf4976de5c39ae72822f
SindhurMarella <167158939+SindhurMarella@users.noreply.github.com>
2024-07-07 18:02:15 +0530

    Update Testimonials.jsx

--- frontend/src/components/Testimonial/Testimonials.jsx
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,104 +1,109 @@
-import React from 'react'
-import Slider from 'react-slick'
-import ava01 from '../../assets/images/ava-1.jpg'
-import ava02 from '../../assets/images/ava-2.jpg'
-import ava03 from '../../assets/images/ava-3.jpg'
+import React from "react";
+import Slider from "react-slick";
+import ava01 from "../../assets/images/ava-1.jpg";
+import ava02 from "../../assets/images/ava-2.jpg";
+import ava03 from "../../assets/images/ava-3.jpg";
+import ava04 from "../../assets/images/ava-4.jpg";
 
 const Testimonials = () => {
-   const settings = {
-      dots:true,
-      infinite:true,
-      autoplay:true,
-      speed:1000,
-      swipeToSlide:true,
-      autoplaySpeed:2000,
-      slidesToShow:3,
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
 
-      responsive: [
-         {
-            breakpoint: 992,
-            settings: {
-               slidesToShow: 2,
-               slidesToScroll: 1,
-               infinite: true,
-               dots: true,
-            },
-            breakpoint: 576,
-            settings: {
-               slidesToShow: 1,
-               slidesToScroll: 1,
-               infinite: true,
-               dots: true,
-            },
-         }
-      ]
-   }
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
 
-   return <Slider {...settings}>
+  return (
+    <Slider {...settings}>
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+          Booking with Travel World was seamless! Their website is
+          user-friendly, and I found great deals on flights and accommodations
+          for my last-minute trip to Paris. Highly recommend!
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava01} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>John Doe</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">James N</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+          I've used Travel World for multiple trips, and they never
+          disappoint. Their customer service is exceptional, and they helped me
+          plan a dream vacation to Southeast Asia within my budget. Will
+          definitely book with them again!
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava02} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>Lia Franklin</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">Elizabeth S</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+          I stumbled upon Travel World while searching for a getaway to the
+          Caribbean. Their recommendations were spot on, and I ended up having
+          the most relaxing vacation in years. Thanks for making travel planning
+          stress-free!
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava03} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>John Doe</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">William T</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias
-            placeat suscipt earum debitits recusandae voluptate illum expedita 
-            corrupti aliquid doloribus delectus?
-         </p>
+        <p>
+          I had an issue with my booking, but the support team at Travel World was incredibly helpful and resolved it quickly. 
+          They went above and beyond to ensure my trip went smoothly. Impressed with their
+          dedication to customer satisfaction!
+        </p>
 
-         <div className='d-flex align-items-center gap-4 mt-3'>
-            <img src={ava03} className='w-25 h-25 rounded-2' alt="" />
-            <div>
-               <h6 className='mb-0 mt-3'>John Doe</h6>
-               <p>Customer</p>
-            </div>
-         </div> 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava04} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">Jack Sparrow</h6>
+            <p>Customer</p>
+          </div>
+        </div>
       </div>
-   </Slider>
-}
+    </Slider>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
